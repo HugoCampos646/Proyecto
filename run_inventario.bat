b8627b3ae43ca3282977cf3aa3794c33985f6fdf
HugoCampos646 <camposlacambrahugo@gmail.com>
2025-05-21 12:45:19 +0200

    acceso directo

--- run_inventario.bat
+++ run_inventario.bat
@@ -1,10 +1,6 @@
 @echo off
 
-set NOMBRE_ENTORNO=env_desarrollo
-set RUTA_PROYECTO=inventario
 set SCRIPT_STREAMLIT=inventario.py
-
-call %NOMBRE_ENTORNO%\Scripts\activate
 
 pushd "%~dp0"
 streamlit run %SCRIPT_STREAMLIT% --server.port=8010
